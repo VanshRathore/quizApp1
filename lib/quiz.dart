9d77eda1a30e3218ec0ba04283a71329f207625e
Vansh Rathore <vanshr083@gmail.com>
2024-05-31 15:46:04 +0530

    first-commit

---
+++ lib/quiz.dart
@@ -0,0 +1,69 @@
+import 'package:app/data/question.dart';
+import 'package:flutter/material.dart';
+import 'package:app/front_page.dart';
+import 'package:app/question_screen.dart';
+import 'package:app/results_screen.dart';
+import 'package:app/models/question_quiz.dart';
+
+class QuizScreen extends StatefulWidget {
+  const QuizScreen({super.key});
+
+  @override
+  State<QuizScreen> createState() => _QuizScreenState();
+}
+
+class _QuizScreenState extends State<QuizScreen> {
+  List<String> selectedAnswer = [];
+  var activeScreen = 'start-screen';
+
+  void switchScren() {
+    setState(
+      () {
+        activeScreen = 'Question-screen';
+      },
+    );
+  }
+
+  void chooseAnswer(answer) {
+    selectedAnswer.add(answer);
+    if (selectedAnswer.length == questions.length) {
+      setState(() {
+        activeScreen = 'results-screen';
+      });
+    }
+  }
+
+  @override
+  Widget build(context) {
+    Widget screenWidget = FrontPage(switchScren);
+
+    if (activeScreen == 'Question-screen') {
+      screenWidget = QuestionScreen(
+        onSelectAnswer: chooseAnswer,
+      );
+    }
+
+    if (activeScreen == 'results-screen') {
+      screenWidget = ResultsScreen(
+        choosenAnswer: selectedAnswer,
+      );
+    }
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 56, 162, 215),
+                Color.fromARGB(255, 86, 17, 57)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: screenWidget,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/results_screen.dart
@@ -0,0 +1,61 @@
+import 'package:app/data/question.dart';
+import 'package:app/question_summary.dart';
+import 'package:flutter/material.dart';
+
+class ResultsScreen extends StatelessWidget {
+  const ResultsScreen({
+    super.key,
+    required this.choosenAnswer,
+  });
+
+  final List<String> choosenAnswer;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < choosenAnswer.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answer[0],
+        'user_answer': choosenAnswer[i],
+      });
+    }
+    return summary;
+  }
+
+  @override
+  Widget build(context) {
+    final summaryData = getSummaryData();
+    final numTotalQuestion = questions.length;
+    final numCorrectAnswer = summaryData.where((data) {
+      return data['user_answer'] == data['correct_answer'];
+    }).length;
+
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'You answer $numCorrectAnswer out of $numTotalQuestion answers correctly!',
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            QuestionSummary(summaryData),
+            const SizedBox(
+              height: 30,
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text('Reset Quiz'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
